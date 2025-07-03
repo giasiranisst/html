057671f5ad89960c45aeff68547d7c7fb4d29acc
giasiranisst <giasiranisst@gmail.com>
2025-07-03 21:19:36 +0300

    Add files via upload

---
+++ a/guess/scripts/script.js
@@ -0,0 +1,119 @@
+const hangmanImage = document.querySelector(".hangman-box img");
+const keyboardDiv = document.querySelector(".keyboard");
+const guessesText = document.querySelector(".guess-text b");
+const wordDisplay = document.querySelector(".word-display");
+const gameModal = document.querySelector(".game-modal");
+const playAgainBtn = document.querySelector(".play-again");
+const sound = document.getElementById('failSound');
+
+let currentWord, currentHint, currentDinop, currentDinoimg, correctLetters, wrongGuessCount;
+const totalWords = 6;
+const maxGuesses = 8;
+let word_id = 0
+let mustguess = 0;
+
+const resetGame = () => {
+    correctLetters = [];
+    wrongGuessCount = 0;
+
+    wordDisplay.innerHTML = currentWord.split("").map((_, i) =>
+        currentHint[i] === "0"
+            ? `<li class="letter"></li>`
+            : `<li class="noguess">${currentWord[i]}</li>`
+    ).join("");
+    gameModal.classList.remove("show");
+    guessesText.innerText = `${wrongGuessCount} από ${maxGuesses}`;
+    keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
+    hangmanImage.src = `img/dinosaur-${wrongGuessCount}.jpg`;
+}
+
+const getNextWord = () => {
+    const { word, hint, dinop, dinoimg } = wordList[word_id];
+    currentWord = word;
+
+    currentHint = hint.split("");
+    mustguess = hint.split("0").length - 1;
+
+    currentDinop = dinop;
+    currentDinoimg = dinoimg;
+
+    resetGame();
+}
+
+const gameOver = (isVictory, nextButton) => {
+    setTimeout(() => {
+        let modaldinop = ``;
+        let modaldinoimg = `img/lost.gif`;
+        let modalText2 = ``;
+        let imgElement = gameModal.querySelector("img");
+
+        if (!imgElement.classList.contains("lost")) {
+            imgElement.classList.add("lost");
+        }
+
+        if (isVictory) {
+            imgElement.classList.remove("lost");
+            modaldinop = `<b>${currentDinop}</b>`;
+            modaldinoimg = `${currentDinoimg}`;
+            modalText2 = `Βρήκες τη λέξη: <b>${currentWord}</b>`;
+        }
+
+        gameModal.querySelector("img").src = modaldinoimg;
+        gameModal.querySelector("h4").innerText = `${isVictory ? 'Μπράβο!' : 'Λυπάμαι, δεν βρήκες τη λέξη.'}`;
+        gameModal.querySelector(".dino").innerHTML = modaldinop;
+        gameModal.querySelector(".found").innerHTML = `${modalText2}`;
+        gameModal.querySelector("button").innerText = `${nextButton ? 'ΕΠΟΜΕΝΗ ΛΕΞΗ' : 'ΑΠΟ ΤΗΝ ΑΡΧΗ'}`;
+        gameModal.classList.add("show");
+    }, 300);
+}
+
+const initGame = (button, clickedLetter) => {
+    let noneHint_0 = true;
+
+    if (currentWord.includes(clickedLetter)) {
+        [...currentWord].forEach((letter, index) => {
+            if (letter === clickedLetter && currentHint[index] === "0") {
+                correctLetters.push(letter);
+                wordDisplay.querySelectorAll("li")[index].innerText = letter;
+                wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+                noneHint_0 = false;
+            }
+        });
+    }
+
+    if (noneHint_0) {
+        sound.currentTime = 0;
+        sound.play();
+        wrongGuessCount++;
+        guessesText.innerText = `${wrongGuessCount} από ${maxGuesses}`;
+        hangmanImage.src = `img/dinosaur-${wrongGuessCount}.jpg`;
+    }
+    button.disabled = true;
+
+    if (wrongGuessCount === maxGuesses) return isLastWord(false);
+    if (correctLetters.length === mustguess) return isLastWord(true);
+}
+
+const isLastWord = (won) => {
+    word_id += 1;
+
+    if (word_id == totalWords) {
+        word_id = 0;
+        return gameOver(won, false);
+    }
+    else
+        return gameOver(won, true);
+}
+
+const letters = "ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ".split("");
+for (let i = 0; i < 24; i++) {
+    const button = document.createElement("button");
+    button.innerText = letters[i];
+    keyboardDiv.appendChild(button);
+
+    button.addEventListener("click", e => initGame(e.target, letters[i]));
+}
+
+getNextWord();
+
+playAgainBtn.addEventListener("click", getNextWord);
